069f2e780d33641be981c4df59c3bad291aa187c
vicentej20 <jorquerav07@gmail.com>
2023-01-07 22:07:07 -0300

    sdd

--- Layout/GeneralLayout.tsx
+++ Layout/GeneralLayout.tsx
@@ -8,7 +8,7 @@ export const GeneralLayout = ({ children, ...props}: Props) => {
   return (
     <main className='min-h-screen'>
       <Navbar />
-      <div className='children flex flex-col gap-5 px-3 mt-14 pt-20 md:mt-4 md:pt-2 md:px-10 py-3 pb-5' {...props}>
+      <div className='children flex flex-col gap-5 px-3 mt-14 pt-20 md:mt-5 md:pt-4 md:px-10 py-3 pb-5' {...props}>
         {children}
       </div>
       
