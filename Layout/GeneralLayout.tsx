6594256ee6cd50a4429b3860a55c68cb197bf5fc
vicentej20 <jorquerav07@gmail.com>
2023-01-08 12:47:09 -0300

    some footer changes

--- Layout/GeneralLayout.tsx
+++ Layout/GeneralLayout.tsx
@@ -7,14 +7,14 @@ interface Props {
 export const GeneralLayout = ({ children, ...props}: Props) => {
   return (
     <>
-      <main className='min-h-screen'>
+      <main className='min-h-screen mb-6'>
         <Navbar />
         <div className='children flex flex-col gap-5 px-3 md:px-10 py-3 pt-5 pb-5 md:pt-2' {...props}>
           {children}
         </div>
       </main>
-      <div className='footer-div text-center py-6 my-2 text-base md:text-lg'>
-        <footer>&copy; Copyright 2023, Vicente Jorquera. Todos los derechos reservados.</footer>
+      <div className='footer-div mb-4 text-center pt-6 pb-6 text-base md:text-lg'>
+        <footer className='px-16'>&copy; Copyright 2023, Vicente Jorquera. Todos los derechos reservados.</footer>
       </div>
     </>
   )
